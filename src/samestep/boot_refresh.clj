4ce93b36657f90b17c4f5a700436400cdce01c3a
Brent Hagany <brent@hiidef.com>
2016-12-16 17:55:09 -0600

    Only attempt to reload namespaces that are already loaded Reloading all of the directories that boot can see results in errors, if those directories contain namespaces that are intended to be used in pods with different dependencies than the main boot environment. By only reloading namespaces that have already been loaded, this implementation avoids running into these missing dependency errors. Credit to @Deraen - I only lightly edited his implementation of this pattern in Perun, here: https://github.com/hashobject/perun/blob/master/src/io/perun/render.clj

--- src/samestep/boot_refresh.clj
+++ src/samestep/boot_refresh.clj
@@ -1,16 +1,35 @@
 (ns samestep.boot-refresh
   {:boot/export-tasks true}
   (:require [boot.core :as boot]
-            [clojure.tools.namespace.repl :as tns]))
+            [boot.util :as util]
+            [clojure.tools.namespace.dir :as dir]
+            [clojure.tools.namespace.track :as track]
+            [clojure.tools.namespace.reload :as reload]))
 
 (boot/deftask refresh
   "Reload all changed namespaces on the classpath.
 
   Throws an exception in the case of failure."
   []
-  (boot/with-pass-thru _
-    (apply tns/set-refresh-dirs (boot/get-env :directories))
-    (with-bindings {#'*ns* *ns*}
-      (let [result (tns/refresh)]
-        (when (instance? Throwable result)
-          (throw result))))))
+  (let [tracker (atom nil)
+        filter-ns (fn [ns] (filter find-ns ns))]
+    (boot/with-pass-thru _
+      (swap! tracker
+             (fn [tracker]
+               (util/dbug "Scan directories: %s\n" (pr-str (boot/get-env :directories)))
+               (-> (or tracker (track/tracker))
+                   (dir/scan-dirs (boot/get-env :directories))
+                   ;; Only reload namespaces which are already loaded
+                   (update ::track/load filter-ns)
+                   (update ::track/unload filter-ns))))
+      (util/info "Unload: %s\n" (pr-str (::track/unload @tracker)))
+      (util/info "Load: %s\n" (pr-str (::track/load @tracker)))
+      (swap! tracker reload/track-reload)
+      (try
+        (when (::reload/error @tracker)
+          (util/fail "Error reloading: %s\n" (name (::reload/error-ns @tracker)))
+          (throw (::reload/error @tracker)))
+        (catch java.io.FileNotFoundException e
+          (util/info "Resetting tracker due to file not found exception, all namespaces will be reloaded next time.\n")
+          (reset! tracker (track/tracker))
+          (throw e))))))
